5ec05a5422f9d26a03b1f7737e8eb25270404397
PandaPalumbo <mattdpalumbo@gmail.com>
2020-03-06 21:38:14 -0500

    more enums. Base CLasses being made

---
+++ Assets/Scripts/Mechanics/Abilities/BaseAbilityClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAbilityClass  
+{
+    private Creature[] targetCreatures;
+    private Vector3 targetSpot;
+    private string abilityName;
+    private EAbilityType abilityType;
+    private EAbilityAction[] abilityActions;
+
+    public Creature[] TargetCreatures { get => targetCreatures; set => targetCreatures = value; }
+    public string AbilityName { get => abilityName; set => abilityName = value; }
+    public Vector3 TargetSpot { get => targetSpot; set => targetSpot = value; }
+    public EAbilityType AbilityType { get => abilityType; set => abilityType = value; }
+    public EAbilityAction[] AbilityActions { get => abilityActions; set => abilityActions = value; }
+}
